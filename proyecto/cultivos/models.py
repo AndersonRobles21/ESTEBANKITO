4d0a2b71915f5c08132b16fce92b1d3778cd65b5
Anderson <andersontopaga2018@gmail.com>
2025-11-25 14:52:51 -0500

    Update models.py

--- proyecto/cultivos/models.py
+++ proyecto/cultivos/models.py
@@ -1,1 +1,11 @@
 
+from django.db import models
+
+class Cultivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    variedad = models.CharField(max_length=100, null=True, blank=True)
+    etapa = models.CharField(max_length=100, null=True, blank=True)
+    fecha_siembra = models.DateField()
+
+    def __str__(self):
+        return self.nombre
