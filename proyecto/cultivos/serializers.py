ac68a72eafabf6fa7aee7c11f8159a3050ab52a5
Anderson <andersontopaga2018@gmail.com>
2025-11-25 14:46:55 -0500

    rama

--- proyecto/cultivos/serializers.py
+++ proyecto/cultivos/serializers.py
@@ -1,7 +1,1 @@
-from rest_framework import serializers
-from .models import Cultivo
 
-class CultivoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cultivo
-        fields = '__all__'
